37ba23c050ad18275da638af3ef2d123411bce86
ismael <ismael@cmpnr-vaio-ubuntu.(none)>
2015-07-12 16:50:09 +0200

    Crear preguntas

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -41,3 +41,21 @@ exports.index = function(req, res) {
 			res.render('quizes/index.ejs', {quizes: quizes});
 		})
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	// crea objeto enlazado a base de datos 
+	// para guardar posteriormente con save()
+	var quiz = models.Quiz.build({pregunta: "Pregunta", respuesta: "Respuesta"});
+	
+	res.render('quizes/new', {quiz: quiz});	
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	quiz.save({fields: ["pregunta", "respuesta"]}).then( function() {
+			res.redirect('/quizes');
+		})
+};
