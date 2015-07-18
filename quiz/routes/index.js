df7cc008ebe888f8efd4c02fb5732e58ab184c05
ismael <ismael@cmpnr-vaio-ubuntu.(none)>
2015-07-18 18:24:00 +0200

    Autenticación y sesión

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res) {
 
 // Autoload para rutas con :quizId
 router.param('quizId', quizController.load);
+
+// Rutas sesión
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy); //deberia ser delete para ser coherente con REST
 
 // Rutas /quizes
 router.get('/quizes', quizController.index);
